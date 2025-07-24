21aaed717377e28841f09a7f9301c96a813d11ae
Andrezinrc <andrem.33333@gmail.com>
2025-07-24 08:35:40 +0000

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const MONGO_URI = process.env.MONGO_URI;
+console.log('URI:', MONGO_URI);
+
+const app = express();
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+
+// Conexão com o MongoDB
+mongoose.connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log('MongoDB conectado com sucesso!');
+}).catch((err) => {
+    console.error('Erro ao conectar no MongoDB:', err);
+});
+
+app.get('/', (req, res) => {
+  res.send('API rodando...');
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
